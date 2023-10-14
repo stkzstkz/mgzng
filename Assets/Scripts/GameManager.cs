88bdd52dc158199adcb8a603ff02c199162028ac
otimuu <sairld0136@gmail.com>
2023-10-15 05:46:01 +0900

    update: create end point of ingame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 using System;
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject EndPoint;
+    [SerializeField]
+    GameObject PlayerObject;
+
     void Awake() {
         GameScoreStatic.Zng=0;
     }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +22,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(PlayerObject.transform.position.z >= EndPoint.transform.position.z)
+        {
+            GameOver();
+        }
     }
+
     public void GameOver(){
         SceneManager.LoadScene("Result");
     }
