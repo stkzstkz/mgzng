d6d74ff9e9b4277fd1b7ddd5633cdc059534b123
stkzstkz <tutti.kazuki@gmail.com>
2023-11-17 06:17:43 +0900

    ランキング実装

--- Assets/Scripts/TestRanking.cs
+++ Assets/Scripts/TestRanking.cs
@@ -6,114 +6,85 @@ using UnityEngine.UI;
 using System.IO;
 using System.Text;
 using TMPro;
-// using rankinglist;
+using System.Linq;
+using System.Text;
+using UnityEngine.SceneManagement;
 
-// namespace rankinglist
-// {
-//     public class Setlist
-//     {
-//         public string name;
-//         public string score;
-//         public Setlist(string name, string score)
-//         {
-//             this.name = name;
-//             this.score = score;
-//         }
-//     }
-// }
+public class Setlist
+{
+    public string names { get; set; }
+    public int scores { get; set; }
+}
 public class TestRanking : MonoBehaviour
 {
-    // System.Collections.Generic.List<rankinglist.Setlist> list = new List<Setlist>();
-    // var ranking = new List<(string name, int score)>();
-    public string[] num = { " 1. ", " 2. ", " 3. ", " 4. ", " 5. ", " 6. ", " 7. ", " 8. ", " 9. ", "10. " };
-    private int[] score = new int[10];
+    public int rankingLength = 10;
     [SerializeField] private Text rankingText;
     [SerializeField] private TMP_InputField input;
-    [SerializeField] private GameObject todestroy;
-    // private static string name = "No Name";
-    private string[] namelist = new string[10];
-    private int Th = -1;
-    private TextAsset csvFile;
+    private List<Setlist> list = new List<Setlist>();
     private List<string[]> csvData = new List<string[]>();
-    private string path = @"Assets/ranking.csv";
-    private int rank;
+    private string path = @"Assets/unchi.csv";
+    private Setlist setlist = new Setlist();
     void Start()
     {
         GetRanking();
-        SetRanking(GameScoreStatic.Zng);
-        ViewRanking();
+        if (SceneManager.GetActiveScene().name == "Ranking")
+        {
+            list.Sort((a, b) => (a.scores - b.scores));
+            list.Reverse();
+            ViewRanking(list.Count);
+        }
+        else
+        {
+            SetRanking(GameScoreStatic.Zng);
+            ViewRanking(rankingLength);
+        }
     }
 
-    // private void Compare(string a[][], string [][]){
-    //     return a[][];
-    // }
     // ランキング呼び出し
     private void GetRanking()
     {
-        // csvFile = Resources.Load("ranking") as TextAsset;
-        // StreamReader sr = new StreamReader(@"E:", Encoding.GetEncoding("Shift_JIS"));
-        // StringReader reader = new StrngReader(csvFile.text);
-        // while (reader.Peek() != -1)
-        // {
-        //     string line = reader.ReadLine(); // 1行ずつ読み込む
-        //     csvData.Add(line.Split(',')); // csvDataリストに追加する
-        // }
-        // var c = new Comparision<string[]>(Compare);
-        // csvData.Sort(c);
         StreamReader sr = new StreamReader(path);
+        int i = 0;
         while (!sr.EndOfStream)
         {
+            Setlist setlist4get = new Setlist();
             string line = sr.ReadLine();
             csvData.Add(line.Split(','));
+            setlist4get.names = csvData[i][0];
+            setlist4get.scores = Int32.Parse(csvData[i][1]);
+            list.Add(setlist4get);
+            i++;
         }
-        //ランキング呼び出し
-        for (int i = 0; i < csvData.Count; i++) // csvDataリストの条件を満たす値の数（全て）
-        {
-            // データの表示
-            Debug.Log("日本語：" + csvData[i][0] + ", English：" + csvData[i][1]);
-        }
-        for (int i = 0; i < csvData.Count; i++)
-        {
-            score[i] = int.Parse(csvData[i][1]);
-            namelist[i] = csvData[i][0];
-        }
-        
     }
     // ランキング書き込み
-    private void SetRanking(int _value)
+    private void SetRanking(int value)
     {
-        // 書き込み要素の記憶
-        int value4namelist = _value;
-        // 書き込み
-        for (int i = 0; i < csvData.Count; i++)
+        setlist.names = "Default";
+        setlist.scores = value;
+        list.Add(setlist);
+        list.Sort((a, b) => (a.scores - b.scores));
+        list.Reverse();
+    }
+
+    public async void SetCsv()
+    {
+        using (StreamWriter sw = new StreamWriter(path, true))
         {
-            //取得した値とRankingの値を比較して入れ替え
-            if (_value > score[i])
-            {
-                var change = score[i];
-                score[i] = _value;
-                _value = change;
-                rank = i;
-                namelist[rank] = "Default";
-                Debug.Log(rank);
-            }
+            sw.WriteLine(setlist.names + "," + setlist.scores);
         }
     }
 
-
     // ランキング表示
-    private void ViewRanking()
+    private void ViewRanking(int length)
     {
         rankingText.text = "";
-        for (int i = 0; i < score.Length; i++)
+        for (int i = 0; i < length; i++)
         {
-            rankingText.text += num[i];
-            if (score[i] != GameScoreStatic.Zng)
-            {
-                rankingText.text += namelist[i];
-            }
+            rankingText.text += i+1;
+            rankingText.text += ". ";
+            rankingText.text += list[i].names;
+            rankingText.text += list[i].scores;
             rankingText.text += " ";
-            rankingText.text += score[i].ToString();
             rankingText.text += "\n";
         }
     }
@@ -121,7 +92,7 @@ public class TestRanking : MonoBehaviour
     // テキストボックスからの読み取り
     public void GetInput()
     {
-        namelist[rank] = input.text;
-        ViewRanking();
+        setlist.names = input.text;
+        ViewRanking(rankingLength);
     }
 }
--- Assets/Scripts/ToScene/ToScene.cs
+++ Assets/Scripts/ToScene/ToScene.cs
@@ -27,4 +27,8 @@ public class ToScene : MonoBehaviour
     {
         SceneManager.LoadScene("Title");
     }
+    public void ToRanking()
+    {
+        SceneManager.LoadScene("Ranking");
+    }
 }
